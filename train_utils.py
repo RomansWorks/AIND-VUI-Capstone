c3195a26eccfb87924edf966bb4762c24b9844bc
Roman Baron <roman.baron.works@gmail.com>
2022-03-13 19:00:00 +0200

    Adjust imports to Google colab

--- train_utils.py
+++ train_utils.py
@@ -5,11 +5,12 @@ Defines a functions for training a NN.
 from data_generator import AudioGenerator
 import _pickle as pickle
 
+from tensorflow import keras
 from keras import backend as K
 from keras.models import Model
 from keras.layers import (Input, Lambda)
-from keras.optimizers import SGD
-from keras.callbacks import ModelCheckpoint   
+from tensorflow.keras.optimizers import SGD
+from keras.callbacks import ModelCheckpoint
 import os
 
 def ctc_lambda_func(args):
